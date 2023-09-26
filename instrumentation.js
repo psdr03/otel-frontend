dbefc191f8024693e2a3496d790a83af7176b450
paolo <psdelarosa@gmail.com>
2023-09-26 18:09:06 +0800

    init

---
+++ instrumentation.js
@@ -0,0 +1,38 @@
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+const { Resource } = require("@opentelemetry/resources");
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+
+// opentelemetry
+const exporter = new OTLPTraceExporter();
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "sho-website",
+  }),
+  spanProcessor: new BatchSpanProcessor(exporter, {
+    maxQueueSize: 2048,
+    maxExportBatchSize: 512,
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-fs": { enabled: false },
+    }),
+  ],
+});
+sdk.start();
+
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Opentelemetry terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Add your custom middleware here, if needed
+
+  // Handle all Next.js requests
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
