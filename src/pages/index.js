3d9e61428d9dccc2ccb2c7c8cd9bcbe94a101243
paolo <psdelarosa@gmail.com>
2023-09-27 11:31:54 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 const callEndpointOnClick = async () => {
   const res = await fetch(
-    "https://vory65wxld.execute-api.ap-southeast-1.amazonaws.com/dev/"
+    "https://5ow5gh3h84.execute-api.ap-southeast-1.amazonaws.com/dev/"
   );
   const data = await res.json();
   console.log(data);
